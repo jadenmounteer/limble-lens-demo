1deec88edd67114361806e0974ea3d5f835d7c95
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-06-23 11:54:32 -0600

    Make ar stuff work

--- src/scripts/deploy-to-github-pages.sh
+++ src/scripts/deploy-to-github-pages.sh
@@ -1,4 +1,4 @@
 cd /Users/jadenmounteer/git/limble-lens-demo
 npm i angular-cli-ghpages --save-prod
-ng build --base-href "https://jadenmounteer.github.io/limble-lens-demo/"
+ng build --base-href "https://jadenmounteer.github.io/limble-lens-demo/ar.js_implementation"
 npx angular-cli-ghpages --dir=dist/limble-lens-demo
